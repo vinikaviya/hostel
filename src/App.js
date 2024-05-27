40683116ec5b17e7bcb2eddf717cbff85bbbca37
vinikaviya <vinikaviya.@gmail.com>
2024-05-27 20:41:36 +0530

    hello commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import Tasklist from './task.js'; 
+
+function App() {
+  
+      <Tasklist />
+ 
+}
+export default App;
---
+++ src/task.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+// import axios from 'axios';
+// import './task.css';
+
+// function Tasklist() {
+//     const [data, setData] = useState([]);
+
+//     useEffect(() => {
+//         axios.get("http://127.0.0.1:8000/api/Task/")
+//             .then(res => setData(res.data))
+//             .catch(error => console.error('Error fetching tasks:', error));
+//     }, []);
+
+//     return (
+//         <div>
+//           <h1>welcome</h1>
+//             {data.map(task => (
+//                 <div key={task.id}>
+//                     {task.title}
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
+
+function Tasklist(){
+    return(
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+export default Tasklist;
+
+
+
